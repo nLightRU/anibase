932c06ee95916a9a4525dd7bf4ab063a035dc96e
Ilya <northenlight6899@gmail.com>
2023-08-31 01:18:35 +0300

    added other data

--- anibase/dataload.py
+++ anibase/dataload.py
@@ -2,10 +2,15 @@ import os
 import sqlite3
 import json
 
-import sqlalchemy.exc
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy import update
+
+db_url = '127.0.0.1:5432'
+db_name = 'anibase_db'
+db_user = 'anibase_app'
+db_pass = 'qwerty12345'
+db_uri = f'postgresql+psycopg2://{db_user}:{db_pass}@{db_url}/{db_name}'
 
 
 def get_organisations(organisation: str) -> list[dict]:
@@ -64,25 +69,106 @@ def get_titles() -> dict:
         for title in titles:
             yield title
 
-def insert_titles(self):
+
+def insert_titles():
 
     from model import Anime
 
     def create_title_row():
         for a in get_titles():
-            anime_title = Anime(mal_id=a['mal_id'], title=a['title'], title_english=a['title_english'],
-                            episodes=a['episodes'], type=a['type'], source=a['source'],
-                            season=a['season'], year=a['year'], rating=a['rating'], synopsis=a['synopsis'],
-                            score=a['score'], members=a['members'])
+            anime_row = Anime(mal_id=a['mal_id'], title=a['title'], title_english=a['title_english'],
+                              episodes=a['episodes'], type=a['type'], source=a['source'],
+                              season=a['season'], year=a['year'], rating=a['rating'], synopsis=a['synopsis'],
+                              score=a['score'], members=a['members'])
 
-            yield anime_title
+            yield anime_row
 
-    engine = create_engine(DBHandler.db_uri)
+    engine = create_engine(db_uri)
     with Session(engine) as session:
-        for a in create_title_row():
-            session.add(a)
+        for anime_title in create_title_row():
+            session.add(anime_title)
         session.commit()
 
+
+def insert_genres():
+    genres_ids = set()
+    genres = []
+    for title in get_titles():
+        for genre in title['genres']:
+            if genre['mal_id'] not in genres_ids:
+                genres_ids.add(genre['mal_id'])
+                genres.append(genre)
+
+        for theme in title['themes']:
+            if theme['mal_id'] not in genres_ids:
+                genres_ids.add(theme['mal_id'])
+                genres.append(theme)
+
+    from model import Genre
+
+    engine = create_engine(db_uri)
+    with Session(engine) as session:
+        for genre in genres:
+            genre_row = Genre(id=genre['mal_id'], name=genre['name'])
+            row = session.query(Genre).filter(Genre.id == genre_row.id)
+            if not row:
+                session.add(genre_row)
+        session.commit()
+
+
+def insert_studios():
+    studio_ids = set()
+    studios = []
+    for title in get_titles():
+        for studio in title['studios']:
+            if studio['mal_id'] not in studio_ids:
+                studio_ids.add(studio['mal_id'])
+                studios.append(studio)
+
+    from model import Studio
+
+    engine = create_engine(db_uri)
+
+    with Session(engine) as session:
+        for studio in studios:
+            s = Studio(id=studio['mal_id'], name=studio['name'])
+            row = session.query(Studio).filter(Studio.id == s.id)
+            if not row:
+                session.add(s)
+
+        session.commit()
+
+
+def insert_anime_studio():
+
+    from model import AnimeStudio
+    id_row = 1
+    engine = create_engine(db_uri)
+    with Session(engine) as session:
+        for title in get_titles():
+            for studio in title['studios']:
+                session.add(AnimeStudio(id=id_row, id_anime=title['mal_id'], id_studio=studio['mal_id']))
+                id_row += 1
+
+        session.commit()
+
+
+def insert_anime_genre():
+
+    from model import AnimeGenre
+    id_row = 1
+    engine = create_engine(db_uri)
+    with Session(engine) as session:
+        for title in get_titles():
+            for genre in title['genres']:
+                session.add(AnimeGenre(id=id_row, id_anime=title['mal_id'], id_genre=genre['mal_id']))
+                id_row += 1
+
+            for theme in title['themes']:
+                session.add(AnimeGenre(id=id_row, id_anime=title['mal_id'], id_genre=theme['mal_id']))
+                id_row += 1
+
+        session.commit()
 
 class DBHandler:
     dir_path = os.path.dirname(os.path.abspath(__file__))
@@ -109,91 +195,6 @@ class DBHandler:
         else:
             self.db_path = os.path.join(DBHandler.data_dir, db_name)
 
-    def insert_genres(self):
-        genres_ids = set()
-        genres = []
-        for title in get_titles():
-            for genre in title['genres']:
-                if genre['mal_id'] not in genres_ids:
-                    genres_ids.add(genre['mal_id'])
-                    genres.append(genre)
-
-            for theme in title['themes']:
-                if theme['mal_id'] not in genres_ids:
-                    genres_ids.add(theme['mal_id'])
-                    genres.append(theme)
-
-        with sqlite3.connect(self.db_path) as con:
-            cur = con.cursor()
-            for genre in genres:
-                select_stmt = 'SELECT id FROM genre WHERE id=?'
-                row = cur.execute(select_stmt, (genre['mal_id'],)).fetchone()
-
-                if not row:
-                    cur.execute('INSERT INTO genre(id, name) VALUES(?, ?)',
-                                (genre['mal_id'], genre['name'])
-                                )
-
-    def insert_studios(self):
-        studio_ids = set()
-        studios = []
-        for title in get_titles():
-            for studio in title['studios']:
-                if studio['mal_id'] not in studio_ids:
-                    studio_ids.add(studio['mal_id'])
-                    studios.append(studio)
-
-        from model import db
-        from model import Studio
-
-        # TO DO: fix path again (if we start not from anibase
-        # it's not opened
-        engine = create_engine('sqlite:///'+self.db_path)
-
-        try:
-            db.metadata.tables['studio'].create(bind=engine)
-        except sqlalchemy.exc.OperationalError:
-            print('Table already exists')
-
-        with Session(engine) as session:
-            for studio in studios:
-                s = Studio(id=studio['mal_id'], name=studio['name'])
-                row = session.query(Studio).filter(Studio.id == s.id)
-                if not row:
-                    session.add(s)
-
-            session.commit()
-
-    def insert_anime_genre(self):
-
-        from model import AnimeGenre
-        id_row = 1
-        engine = create_engine('sqlite:///' + self.db_path)
-        with Session(engine) as session:
-            for title in get_titles():
-                for genre in title['genres']:
-                    session.add(AnimeGenre(id=id_row, id_anime=title['mal_id'], id_genre=genre['mal_id']))
-                    id_row += 1
-
-                for theme in title['themes']:
-                    session.add(AnimeGenre(id=id_row, id_anime=title['mal_id'], id_genre=theme['mal_id']))
-                    id_row += 1
-
-            session.commit()
-
-    def insert_anime_studio(self):
-
-        from model import AnimeStudio
-        id_row = 1
-        engine = create_engine('sqlite:///' + self.db_path)
-        with Session(engine) as session:
-            for title in get_titles():
-                for studio in title['studios']:
-                    session.add(AnimeStudio(id=id_row, id_anime=title['mal_id'], id_studio=studio['mal_id']))
-                    id_row += 1
-
-            session.commit()
-
     def update_stats(self):
         from model import Anime
         engine = create_engine('sqlite:///' + self.db_path)
@@ -211,9 +212,9 @@ class DBHandler:
 if __name__ == '__main__':
     # print(*get_organisations('studios'), sep='\n')
     handler = DBHandler('anime_db.sqlite')
-    insert_titles()
-    # handler.insert_genres()
-    # handler.insert_studios()
-    # handler.insert_anime_genres()
-    # handler.insert_anime_studio()
+    # insert_titles()
+    # insert_genres()
+    # insert_studios()
+    # insert_anime_studio()
+    # insert_anime_genre()
     # handler.update_stats()
