4f28b0bb3577bc7cee2a3a9ebaf8fa8a546c4ff2
Ilya <northenlight6899@gmail.com>
2023-08-30 13:25:23 +0300

    ad hoc genres on anime page

--- anibase/anime.py
+++ anibase/anime.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
-from .model import db, Anime
+from sqlalchemy import select
+
+from .model import db, Anime, Genre, AnimeGenre
 
 anime = Blueprint('anime', __name__, url_prefix='/')
 
@@ -7,7 +9,14 @@ anime = Blueprint('anime', __name__, url_prefix='/')
 @anime.route('/anime/id/<int:mal_id>')
 def anime_by_id(mal_id):
     anime_title = db.get_or_404(Anime, mal_id)
-    return render_template('anime.html', anime=anime_title)
+    title_genres = db.session.execute(select(AnimeGenre).where(AnimeGenre.id_anime == mal_id))
+    print(title_genres)
+    genres = []
+    for tg in title_genres:
+        # print(tg[0].id_genre)
+        genres.append(db.get_or_404(Genre, tg[0].id_genre))
+
+    return render_template('anime.html', anime=anime_title, genres=genres)
 
 
 @anime.route('/anime/year/<int:year_val>')
