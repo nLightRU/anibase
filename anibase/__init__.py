771a090427a4a2d3a611dd46fe338ff34f2593d8
Ilya <northenlight6899@gmail.com>
2023-08-31 21:07:02 +0300

    added some views

--- anibase/__init__.py
+++ anibase/__init__.py
@@ -6,14 +6,10 @@ def create_app():
 
     app = Flask(__name__)
 
-    from .anime import anime
+    from .views import views
     from .users import users
 
-    @app.route('/')
-    def home():
-        return '<h1>Homepage</h1>'
-
-    app.register_blueprint(anime)
+    app.register_blueprint(views)
     app.register_blueprint(users)
 
     # Database setup
---
+++ anibase/views.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, render_template
+from flask import abort
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from .model import Anime, Genre, Studio, AnimeGenre
+from .model import engine
+
+views = Blueprint('views', __name__, url_prefix='/')
+
+
+@views.route('/')
+def index():
+    return render_template('index.html')
+
+
+@views.route('/anime')
+def anime():
+    with Session(engine) as session:
+        anime_list = session.execute(select(Anime)
+                                     .order_by(Anime.members.desc())
+                                     .limit(20), execution_options={"prebuffer_rows": True}).scalars()
+    return render_template('anime_index.html', anime_titles=anime_list)
+
+
+@views.route('/anime/<int:mal_id>')
+def anime_by_id(mal_id):
+    genres = []
+    with Session(engine) as session:
+        try:
+            anime_title = session.get(Anime, mal_id)
+            anime_genres = session.execute(select(AnimeGenre).where(AnimeGenre.id_anime == mal_id))
+            for ag in anime_genres.scalars():
+                genres.append(session.get(Genre, ag.id_genre))
+            session.close()
+        except Exception as ex:
+            print(ex)
+            abort(404)
+
+    return render_template('anime_id.html', anime=anime_title, genres=genres)
+
+
+@views.route('/anime/year/<int:year_val>')
+def anime_by_year(year_val):
+    with Session(engine) as session:
+        anime_titles = session.query(Anime).filter(Anime.year == year_val)
+        anime_titles = sorted(anime_titles, key=lambda a: 0 if not a.score else a.score, reverse=True)
+        session.close()
+    return render_template('anime_year.html', titles=anime_titles, year=year_val)
+
+
+@views.route('/studios')
+def studios():
+    with Session(engine) as session:
+        anime_studios = session.query(Studio)
+        # print(studios)
+        session.close()
+    return render_template('studios.html', studios=anime_studios)
