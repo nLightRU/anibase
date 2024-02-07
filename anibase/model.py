675b3484b6370bd305b76a5f8ba074057b673ea0
Ilya <northenlight6899@gmail.com>
2024-02-07 22:02:55 +0300

    add constraint naming conventions to the Base

--- anibase/model.py
+++ anibase/model.py
@@ -1,7 +1,7 @@
 from typing import Optional
 import os
 
-from sqlalchemy import create_engine, URL
+from sqlalchemy import create_engine, URL, MetaData
 from sqlalchemy import Integer, Float, String, Text, Sequence
 from sqlalchemy import ForeignKey
 
@@ -24,7 +24,18 @@ db_url = URL.create(
 )
 
 engine = create_engine(db_url)
-Base = declarative_base()
+
+meta = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
+)
+
+Base = declarative_base(metadata=meta)
 
 
 class Anime(Base):
